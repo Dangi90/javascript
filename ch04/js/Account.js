50b97a313542879be311fdbb5931fbf84389f540
woo <woo24465@gmail.com>
2024-08-08 16:40:24 +0900

    8/8

---
+++ ch04/js/Account.js
@@ -0,0 +1,28 @@
+class Account {
+
+   
+
+    constructor(bank, id, name, balance){
+        this.bank = bank;
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    deposit(money){
+        this.balance += money;
+    }
+
+    
+    withdraw(money){
+        this.balance -= money;
+    }
+
+    show(){
+        document.write('<p>');
+        document.write('은행: ' +this.bank + '<br>');
+        document.write('아이디: ' +this.id + '<br>');
+        document.write('이름: ' +this.name + '<br>');
+        document.write('잔액: ' +this.balance + '<br>');
+    }
+}
